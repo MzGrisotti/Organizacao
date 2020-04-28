8a510499e4c30a9197fb9def931748e4a7e6e4c7
Matheus Grisotti <mzgrisotti@inf.ufsm.br>
2020-04-28 18:04:48 -0300

    Arquivos adicionados

---
+++ main.asm
@@ -0,0 +1,98 @@
+.data
+	myFile: .asciiz "C:\\Users\\mathe\\Documents\\curso\\organização\\trabalho 01\\text.bin"      # filename for input
+	buffer: .space 4000
+	newline: .asciiz "\n"
+	done: .asciiz "\n Programa terminado \n"
+	pc: .word 0
+
+.text
+	main:
+	
+		la $a1, myFile	#posiciona o arquivo
+		jal openfile	#abre o arquivo
+		
+		addi	$sp, $sp, -4
+		sw		$v1, 0($sp)
+		#move $s0, $v1   #salva o descritor do arquivo retornado da função
+		
+		jal readfile	#le o arquivo
+		
+		li $v0, 4
+		la $a0, done
+		syscall
+		
+		li $v0, 10      # Finish the Program
+		syscall
+		
+	
+	openfile:
+		li   $v0, 13          # id para abrir um arquivo
+		move $a0, $a1      	  # nome da variavel
+		li   $a1, 0           # flag para leitura
+		li   $a2, 0           # não usado
+		syscall               # abre o arquivo
+		move $v1, $v0         # retorna o descritor do arquivo
+		
+		jr $ra  
+	
+	readfile:
+	
+		move $s0, $ra # guardar o registrador de jump
+		
+		
+		li   $v0, 14        # id para ler arquivo							
+		lw   $a0, 0($sp)	# descritor do arquivo
+		la   $a1, buffer    # endereço do buffer
+		li   $a2, 4000      # quantidade de bytes para ler
+		syscall             # lê o arquivo
+			
+		la $t0, buffer		# carrega o buffer
+		while:
+			
+			lw $t6, pc			# carrega o pc
+			lw $t2, buffer($t6) # pega a instrução na posição pc
+			beqz $t2, exit		# se for zero significa que acabou
+			move $a1, $t2		# posiciona a instrução em decimal
+			jal print_binary	# transforma em binario
+			
+			addi $t6, $t6, 4	# incrementa pc em 4
+			sw $t6, pc			# salva pc
+			
+			#printa nova linha
+			li $v0, 4
+			la $a0, newline
+			syscall
+		
+			j while		#repete o loop
+	
+		exit:
+			move $ra, $s0 	#recupera o endereço de jump
+			jr $ra			#jump
+	
+		
+	
+	print_binary:
+	
+		move	$t0, $a1
+		addi	$t1, $zero, 31	# constant shift amount
+		addi	$t2, $zero, 0	# variable shift amount
+		addi	$t3, $zero, 32	# exit condition
+		
+		print_binary_loop:
+			beq	$t2, $t3, return
+		
+			sllv	$a0, $t0, $t2
+			srlv	$a0, $a0, $t1
+			
+			
+		
+			li	$v0, 1		# PRINT_INT
+			syscall
+		
+			addi	$t2, $t2, 1
+			j	print_binary_loop
+		
+		return:
+		
+			jr $ra
+	
